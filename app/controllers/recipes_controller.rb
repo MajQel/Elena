da3f006aa8f6fd2beeadc8b12afc63045f59b121
MajQel <Despirage@gmail.com>
2017-10-13 13:29:17 +0200

    before extending recipe table by portions and preparation_time

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -42,7 +42,7 @@ class RecipesController < ApplicationController
 	private
 
 	def recipe_params
-		params.require(:recipe).permit(:tittle, :description, :image)
+		params.require(:recipe).permit(:tittle, :description, :image, ingredients_attributes: [:id, :name, :_destroy], directions__attributes: [:id, :step, :_destroy], recipe_ingredients_attributes: [:id, :quantity, :_destroy])
 	end
 
 	def find_recipe
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,18 @@
 class Recipe < ApplicationRecord
-		
+	has_many :directions
+	has_many :recipe_ingredients
+	has_many :ingredients, through: :recipe_ingredients
+
+	accepts_nested_attributes_for :ingredients,
+									reject_if: proc { |attributes| attributes['name'].blank? },
+									allow_destroy: true
+	accepts_nested_attributes_for :directions,
+									reject_if: proc { |attributes| attributes['step'].blank? },
+									allow_destroy: true
+	accepts_nested_attributes_for :recipe_ingredients,
+									reject_if: proc { |attributes| attributes['quantity'].blank? },
+									allow_destroy: true
+	
 	validates :tittle, :description, :image, presence: true
     has_attached_file :image, styles: { :medium => "400x400#" }
     validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
